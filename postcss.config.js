8e037803ec309c949e7c07b7c118d40bc845934e
Filip Wachowiak <filipw01@interia.eu>
2020-06-17 22:43:31 +0200

    Add right-0 and left-0 to purgecss whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
       ? {
           "@fullhuman/postcss-purgecss": {
             content: ["./components/**/*.js", "./pages/**/*.js"],
-            whitelist: ["origin-left", "origin-right"],
+            whitelist: ["origin-left", "origin-right", "right-0", "left-0"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
