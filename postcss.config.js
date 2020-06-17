017686639dec36df326c2f5ed60eeab1dc60f9d9
Filip Wachowiak <filipw01@interia.eu>
2020-06-17 22:39:49 +0200

    Add orogin-left and origin-right to purgecss whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,7 @@ module.exports = {
       ? {
           "@fullhuman/postcss-purgecss": {
             content: ["./components/**/*.js", "./pages/**/*.js"],
+            whitelist: ["origin-left", "origin-right"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
